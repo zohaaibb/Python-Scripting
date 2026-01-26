884c28ba54167c0fec1a60eb493bc65c1bf21a67
Muhammad Zohaib Zafar <168782250+zohaaibb@users.noreply.github.com>
2026-01-26 13:12:49 +0500

    Add files via upload

---
+++ Day 5 - Command Line Argument/Command-line.py
@@ -0,0 +1,14 @@
+import sys
+
+if len(sys.argv) < 2:
+    print("Usage: python script.py <name>")
+    sys.exit()
+
+name = sys.argv[1]
+print(f"Hello, {name}")
+
+
+# To Run this you gotta type the below in command line
+python script.py name
+ #write name any you want to give as input to the program
+ 
